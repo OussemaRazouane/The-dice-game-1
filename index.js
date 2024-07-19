18d7331e3b652b63d7f798e809c28c6a9bf62e0a
Oussema Razouane <oussemarznpro@gmail.com>
2024-07-19 22:08:57 +0100

    Dicee game 1 V1.0

---
+++ index.js
@@ -0,0 +1,12 @@
+let randomNumber1=Math.trunc(Math.random()*5+1)
+let randomNumber2=Math.trunc(Math.random()*5+1)
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png")
+document.querySelector(".img2").src="./images/dice"+randomNumber2+".png"
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Player 1 Wins! 🚩"
+}else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent="Player 2 Wins! 🚩"
+}else{
+    document.querySelector("h1").textContent="Draw!"
+}
